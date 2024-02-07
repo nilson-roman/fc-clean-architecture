57f4ad522a47c844b3da442d3d7330fb531cefa0
Nilson Roman <nilsonoroman@gmail.com>
2024-02-07 20:47:06 -0300

    test: add integration test for create product use case

---
+++ src/usecase/product/create/create.product.integration.spec.ts
@@ -0,0 +1,50 @@
+import { v4 as uuid } from "uuid";
+import { Sequelize } from "sequelize-typescript";
+
+import CreateProductUseCase from "./create.product.usecase";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import Product from "../../../domain/product/entity/product";
+
+describe("Integration test create product use case", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+
+        await sequelize.addModels([ProductModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("should create a product", async () => {
+        const productRepository = new ProductRepository();
+        const useCase = new CreateProductUseCase(productRepository);
+
+        const product = new Product(uuid(), "Product 1", 10);
+
+        const input = {
+            type: "a",
+            name: "Product 1",
+            price: 10
+        }
+        
+        await productRepository.create(product);
+
+        const output = await useCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price
+        });
+    })
+})
